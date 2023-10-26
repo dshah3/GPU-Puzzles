870e188de51e3775122875b118a504dd632781a4
Devin Shah <dshah3@outlook.com>
2023-10-26 14:13:29 -0400

    GPU puzzles c++ executors

---
+++ GPU_puzzlers_exec/blocks_runner.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cassert>
+#include <cuda_runtime.h>
+
+extern __global__ void Blocks(float* A, float* C, float size);
+
+void runKernel() {
+    const int size = 5;
+    float A[size], C[size];
+
+    for (int i = 0; i < size; i++) {
+        A[i] = static_cast<float>(i);
+    }
+
+    float *d_A, *d_C;
+
+    cudaMalloc(&d_A, size * sizeof(float));
+    cudaMalloc(&d_C, size * sizeof(float));
+
+    cudaMemcpy(d_A, A, size * sizeof(float), cudaMemcpyHostToDevice);
+
+    int threadsPerBlock = size - 1;
+    int blocksPerGrid = (size + threadsPerBlock - 1) / threadsPerBlock;
+
+    Blocks<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_C, size);
+
+    cudaMemcpy(C, d_C, size * sizeof(float), cudaMemcpyDeviceToHost);
+
+    cudaFree(d_A);
+    cudaFree(d_C);
+
+    for (int i = 0; i < size; i++) {
+        assert(C[i] == A[i] + 10);
+    }
+
+    std::cout << "Blocks successful!" << std::endl;
+    return 0;
+}
+
+int main() {
+    runKernel();
+    return 0;
+}
---
+++ GPU_puzzlers_exec/broadcast_runner.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cassert>
+#include <cuda_runtime.h>
+
+extern __global__ void Broadcast(float* A, float* B, float* C, int size);
+
+void runKernel() {
+    const int size = 4;
+    float A[size][1], B[1][size], C[size][size];
+
+    for (int i = 0; i < size; i++) {
+        A[i][0] = static_cast<float>(i);
+    }
+
+    for (int j = 0; j < size; j++) {
+        B[0][j] = static_cast<float>(j);
+    }
+
+    float *d_A, *d_B, *d_C;
+
+    cudaMalloc(&d_A, size * sizeof(float));
+    cudaMalloc(&d_B, size * sizeof(float));
+    cudaMalloc(&d_C, (size * size) * sizeof(float));
+
+    cudaMemcpy(d_A, A, size * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, B, size * sizeof(float), cudaMemcpyHostToDevice);
+
+    dim3 blockDim(size, size);
+
+    Broadcast<<<1, blockDim>>>(d_A, d_B, d_C, size);
+
+    cudaMemcpy(C, d_C, (size * size) * sizeof(float), cudaMemcpyDeviceToHost);
+
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+        assert(C[i][j] == A[i][0] + B[0][j]);
+        }
+    }
+
+    std::cout << "Broadcast successful" << std::endl;
+    return 0;
+}
+
+int main() {
+    runKernel();
+    return 0;
+}
